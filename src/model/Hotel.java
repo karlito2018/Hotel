5cbc0db1a73654239d85aaffab5604e8815bfcf4
karlito2018 <karlito.jishkariani@sangu.edu.ge>
2019-03-25 20:51:03 +0400

    Initial commit

---
+++ src/model/Hotel.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hotel {
+    private String name;
+    private String country;
+    private String city;
+    private double stars;
+    private List<Room> rooms=new ArrayList<>();
+
+    public Hotel(String name, String country, String city, double stars) {
+        this.name = name;
+        this.country = country;
+        this.city = city;
+        this.stars = stars;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getStars() {
+        return stars;
+    }
+
+    public void setStars(double stars) {
+        this.stars = stars;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+    public void addRoom(Room room)
+    {
+        rooms.add(room);
+    }
+}
